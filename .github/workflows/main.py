b82ca45f5163464e07ec11658888ec571c93c5cf
Angelsebastian0 <ancuaran@unal.edu.co>
2024-11-12 11:40:51 -0500

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -5,40 +5,36 @@ class Asiento:
         self.registro = registro
 
     def cambiarColor(self, color):
-        while True:
-            if color == "rojo" or color == "verde" or color == "negro" or color == "amarillo" or color == "blanco":
-                self.color = color
-                break
-            else: 
-                continue
+        if color in ["rojo", "verde", "negro", "amarillo", "blanco"]:
+            self.color = color
+
 
 class Auto:
     cantidadCreados = 0
-    def __init__(self, precio, modelo, asientos, marca, motor, registro, cantidadCreados):
+    
+    def __init__(self, precio, modelo, asientos, marca, motor, registro):
         self.precio = precio
         self.modelo = modelo 
         self.asientos = asientos
         self.marca = marca
-        self.motor = motor
+        self.motor = motor   
         self.registro = registro
         Auto.cantidadCreados += 1
 
-        
     def cantidadAsientos(self):
-        numAsientos = sum(1 for asiento in self.asientos if asiento is not None)
-        return numAsientos
-        
+        return sum(1 for asiento in self.asientos if asiento is not None)
     
     def verificarIntegridad(self):
         if self.motor.registro != self.registro:
             return "Las piezas no son originales"
-    
+        
         for asiento in self.asientos:
             if asiento is not None and asiento.registro != self.registro:
                 return "Las piezas no son originales"
 
         return "Auto original"
         
+
 class Motor:
     def __init__(self, numeroCilindros, tipo, registro):
         self.numeroCilindros = numeroCilindros
@@ -49,15 +45,8 @@ class Motor:
         self.registro = registro
 
     def asignarTipo(self, tipo):
-        while True:
-            if tipo == "electrico" or tipo == "gasolina":
-                self.tipo = tipo
-                break
-            else:
-                continue
-
-
-
+        if tipo in ["electrico", "gasolina"]:
+            self.tipo = tipo
 
 
 
